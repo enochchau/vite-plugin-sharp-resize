ad9b6cf2777c1225e9748190adc486dc33147f05
ec965 <22567131+ec965@users.noreply.github.com>
2022-03-23 21:59:00 -0700

    init

---
+++ src/aspect-ratio.ts
@@ -0,0 +1,34 @@
+import { AspectRatio, AspectRatioStr } from "./types";
+
+export function parseAspectRatio(aspectRatio: AspectRatioStr): AspectRatio {
+  const [arW, arH] = aspectRatio.split(":");
+  const w = parseInt(arW);
+  const h = parseInt(arH);
+  return { w, h };
+}
+
+/**
+ * @param height input height
+ * @param aspectRatio input aspect ratio
+ * @returns computed width
+ */
+export function getAspectWidth(
+  height: number,
+  aspectRatio: AspectRatioStr
+): number {
+  const { w, h } = parseAspectRatio(aspectRatio);
+  return Math.round(height * (w / h));
+}
+
+/**
+ * @param width input width
+ * @param aspectRatio input aspect ratio
+ * @returns computed height
+ */
+export function getAspectHeight(
+  width: number,
+  aspectRatio: AspectRatioStr
+): number {
+  const { w, h } = parseAspectRatio(aspectRatio);
+  return Math.round(width * (h / w));
+}
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+// Aspect Ratio helpers
+export type AspectRatioStr = `${number}${":" | "/"}${number}`;
+export type AspectRatio = { w: number; h: number };
